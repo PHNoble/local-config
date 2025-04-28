26c773fc416a10950702ea7430c7ab86350de2b2
Paul Noble <ph98noble@me.com>
2025-04-27 20:21:00 -0400

    Add minimap plugin

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,7 +1,7 @@
 require "nvchad.mappings"
 local ts_builtin = require("telescope.builtin")
 -- add yours here
-
+vim.keymap.del("n", "<leader>n")
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
@@ -12,3 +12,4 @@ map("n", "<leader>gd", ts_builtin.lsp_definitions, { desc = "Go to definition" }
 map("n", "<leader>gr", ts_builtin.lsp_references, { desc = "Go to references" })
 map("n", "<leader>gi", ts_builtin.lsp_implementations, { desc = "Go to implementations" })
 map("n", "<leader>gx", ts_builtin.lsp_type_definitions, { desc = "Go to type definitions" })
+map("n", "<leader>gk", vim.diagnostic.open_float, { desc = "Open floating lsp errors"})
